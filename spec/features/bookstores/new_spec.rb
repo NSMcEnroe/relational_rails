9580ef5d92a8cb9e508b19eaad13c23cc1a91c24
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-27 23:14:19 -0600

    Edit: add curent progress for unit story 11, night night

--- spec/features/bookstores/new_spec.rb
+++ spec/features/bookstores/new_spec.rb
@@ -3,9 +3,30 @@ require 'rails_helper'
 RSpec.describe 'creating new bookstores' do
   it 'links to the new page from the parent index page' do
     visit '/bookstores'
-    
+
     click_link('New Bookstore')
     expect(current_path).to eq('/bookstores/new')
   end
+
+  it 'can create a new bookstore'
+    visit '/bookstores/new'
+
+    fill_in('name', with: "Danika's Den")
+    fill_in('currently_open', with: "true")
+    fill_in('revenue', with: 125.75)
+    fill_in('opening', with: "2000-12-13 11:15:00")
+    fill_in('fiscal_end_year', "2023-02-01 16:00:00")
+    click_button('Create Bookstore')
+
+    new_bookstore_id = Bookstore.last.id
+    expect(current_path).to eq("/bookstores/#{new_bookstore_id}")
+    expect(page).to have_content("Danika's Den")
+    expect(page).to have_content("true")
+    expect(page).to have_content(125.75)
+    expect(page).to have_content("2000-12-13 11:15:00")
+    expect(page).to have_content("2023-02-01 16:00:00")
+
+  end
+
 end
 
