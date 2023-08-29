83401589b31dafd76878d77162f2d1a3307050a1
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-29 13:44:18 -0600

    Feat: Add delete button to the child index page

--- spec/features/books/destroy_spec.rb
+++ spec/features/books/destroy_spec.rb
@@ -25,4 +25,29 @@ RSpec.describe 'destroying a book' do
     expect(current_path).to eq('/books')
     expect(page).to_not have_content("Good Omens")
   end
+
+  it 'can delete the book from the index page' do
+    bookstore_1 = Bookstore.create(
+      name: "Honey's Home", 
+      currently_open: true, 
+      revenue: 70.5, 
+      opening: "2018-08-01 10:00:00", 
+      fiscal_end_year: "2023-01-31 20:00:00")
+    
+    book_1 = bookstore_1.books.create(
+      title: "Good Omens",
+      author: "Terry Pratchett",
+      fiction: true,
+      cost: 17.99,
+      date_published: "1990-05-10 09:30:00",
+      last_sold: "2023-09-10 13:00:00"
+      )
+
+    visit "/books"
+
+    click_button 'Delete Book'
+
+    expect(current_path).to eq('/books')
+    expect(page).to_not have_content("Good Omens")
+  end
 end
