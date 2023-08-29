986c1adf0c4b76b2b32e96e47719963a2aa9c8b8
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-28 22:46:38 -0600

    Edit: finish user story 15, where only fiction books are shown in the index

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe 'the childs index page' do
     book_2 = bookstore_1.books.create(
       title: "A Short History",
       author: "Bill Bryson",
-      fiction: false,
+      fiction: true,
       cost: 19.99,
       date_published: "2004-02-01 08:30:00",
       last_sold: "2023-11-20 11:25:00" 
@@ -43,6 +43,39 @@ RSpec.describe 'the childs index page' do
       expect(page).to have_content(book_2.cost)
       expect(page).to have_content(book_2.date_published)
       expect(page).to have_content(book_2.last_sold)
+  end
+
+  it "displays only books that are fiction" do
+    bookstore_1 = Bookstore.create(
+      name: "Honey's Home", 
+      currently_open: true, 
+      revenue: 70.5, 
+      opening: "2018-08-01 10:00:00", 
+      fiscal_end_year: "2023-01-31 20:00:00")
+    
+    book_1 = bookstore_1.books.create(
+      title: "Good Omens",
+      author: "Terry Pratchett",
+      fiction: true,
+      cost: 17.99,
+      date_published: "1990-05-10 09:30:00",
+      last_sold: "2023-09-10 13:00:00"
+      )
+    
+    book_2 = bookstore_1.books.create(
+      title: "A Short History",
+      author: "Bill Bryson",
+      fiction: false,
+      cost: 19.99,
+      date_published: "2004-02-01 08:30:00",
+      last_sold: "2023-11-20 11:25:00" 
+      )
+      
+      visit "/books"
+
+      expect(page).to have_content(book_1.title)
+
+      expect(page).not_to have_content(book_2.title)
   end
 end
 
