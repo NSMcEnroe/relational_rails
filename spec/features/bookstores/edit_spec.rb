fc325379571a33abc3afdfcddacdf0695895d3d6
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-29 08:14:16 -0600

    Feat: Add update bookstore to show page

--- spec/features/bookstores/edit_spec.rb
+++ spec/features/bookstores/edit_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "updating a bookstore" do
-  it 'links to the edit page' do
+  it 'links to the edit page from the show page' do
+    bookstore = Bookstore.create!(
+      name: "Danika's Den",
+      currently_open: true,
+      revenue: 20,
+      opening: "2000-12-13 11:15:00",
+      fiscal_end_year: "2023-02-01 16:00:00"
+      )
+    
+      visit "/bookstores/#{bookstore.id}"
+
+      click_button "Update #{bookstore.name}"
+
+      expect(current_path).to eq("/bookstores/#{bookstore.id}/edit")
+  end
+
+  it 'links to the edit page from the index page' do
     bookstore = Bookstore.create!(
       name: "Danika's Den",
       currently_open: true,
