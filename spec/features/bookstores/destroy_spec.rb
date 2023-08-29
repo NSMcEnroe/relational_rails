e63f34bd381b7aa436de9906996f7cac608fd9de
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-29 13:38:13 -0600

    Feat: Add delete button to parent index page

--- spec/features/bookstores/destroy_spec.rb
+++ spec/features/bookstores/destroy_spec.rb
@@ -16,6 +16,22 @@ RSpec.describe 'destroying a bookstore' do
     expect(current_path).to eq('/bookstores')
     expect(page).to_not have_content("Danika's Den")
   end
+
+  it 'can delete the bookstore from the index page' do
+    bookstore = Bookstore.create!(
+      name: "Danika's Den",
+      currently_open: true,
+      revenue: 20,
+      opening: "2000-12-13 11:15:00",
+      fiscal_end_year: "2023-02-01 16:00:00"
+      )
+
+    visit "/bookstores/"
+    click_button 'Delete Bookstore'
+
+    expect(current_path).to eq('/bookstores')
+    expect(page).to_not have_content("Danika's Den")
+  end
 end
 
 
